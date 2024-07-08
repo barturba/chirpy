57404df748b59c27b524cc94a568b77a757f558a
barturba <b@bartas.co>
2024-07-08 14:07:35 -0600

    refactor: Improve chirp validation error handling

--- validate.go
+++ validate.go
@@ -22,22 +22,9 @@ func handlerValidate(w http.ResponseWriter, r *http.Request) {
     }
 
 	bodyLen := len(params.Body)
+
 	if bodyLen > 140 {
-		type returnValsError struct {
-        	Error string `json:"error"`
-    	}
-    	respBodyError := returnValsError{
-			Error: "Chirp is too long",
-    	}
-    	dat, err := json.Marshal(respBodyError)
-		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			w.WriteHeader(500)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
-		w.Write(dat)
+		respondWithError(w, 400, "Chirp is too long")
 		return
 	}
 
@@ -47,14 +34,37 @@ func handlerValidate(w http.ResponseWriter, r *http.Request) {
     respBody := returnVals{
 		Valid: true,
     }
-    dat, err := json.Marshal(respBody)
+	respondWithJSON(w, http.StatusOK, respBody)
+	return
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+		type returnValsError struct {
+        	Error string `json:"error"`
+    	}
+    	respBodyError := returnValsError{
+			Error: msg,
+    	}
+    	dat, err := json.Marshal(respBodyError)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(code)
+		w.Write(dat)
+		return
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    dat, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
 		return
 	}
     w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(200)
+    w.WriteHeader(code)
 	w.Write(dat)
-
 }

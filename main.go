5738c0671fa2b451739ca841f2f2d1324524d02e
barturba <b@bartas.co>
2024-07-08 11:34:08 -0600

    Add file server and health endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net/http"
 )
 
@@ -10,8 +11,16 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthHandler)
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(200)
+	io.WriteString(w, "OK")
 }
